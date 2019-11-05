21306ac2413373c442a211890b953b1c614a84d1
carrie91233 <57000421+carrie91233@users.noreply.github.com>
2019-11-05 10:30:54 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+        public class MyClass {
+         public  static void main(String[] args){
+         System.out.print("hello world + 3a513115");
+
+        }
+
+        }
